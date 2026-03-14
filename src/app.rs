82eafe15aaea45897fad6477fd83141cf65af3af
Linus-Mussmaecher <linus@mussmaecher.de>
2026-03-14 23:06:24 +0100

    Initial commit

---
+++ src/app.rs
@@ -0,0 +1,8 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <h1>"Refraktor"</h1>
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![warn(missing_docs)]
+
+//! The aim of this crate is to generate 4-voiced SATB-sheets from predefined accords.
+//! Especially, the original python logic was translated to rust and is used to create a web application.
+
+mod app;
+use app::App;
+// Contains the underlying logic, note calculation, chorsatz generation, etc.
+mod logic;
+
+fn main() {
+    leptos::mount::mount_to_body(|| leptos::view! {<App/>})
+}
